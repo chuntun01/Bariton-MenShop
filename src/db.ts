84a3bd5c261af49ae2316289bfbd2d83f2412ac2
trung tuan <hotrungtuan18062004@gmail.com>
2025-10-05 02:25:07 +0700

    2.Use DATABASE_URL for Vercel PostgreSQL connection

--- src/db.ts
+++ src/db.ts
@@ -3,7 +3,8 @@ import { Pool } from "pg";
 const dbConnect = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false, // cần thiết khi deploy lên Vercel hoặc dùng Supabase
+    require: true,
+    rejectUnauthorized: false, // bỏ kiểm tra chứng chỉ tự ký
   },
 });
 
